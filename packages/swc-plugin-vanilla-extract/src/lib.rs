5f26a9ebc1c8a99a930e6bd5aa17e2f8122459a8
OJ Kwon <kwon.ohjoong@gmail.com>
2022-09-09 15:00:53 -0700

    feat(plugin): initial setup

---
+++ packages/swc-plugin-vanilla-extract/src/lib.rs
@@ -0,0 +1,27 @@
+use swc_core::{
+    ecma::{ast::Program, visit::*},
+    plugin::{
+        metadata::TransformPluginMetadataContextKind, plugin_transform,
+        proxies::TransformPluginProgramMetadata,
+    },
+};
+
+use swc_vanilla_extract_visitor::create_extract_visitor;
+
+#[plugin_transform]
+pub fn process(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
+    let filename = metadata.get_context(&TransformPluginMetadataContextKind::Filename);
+    let filename = if let Some(filename) = filename.as_deref() {
+        filename
+    } else {
+        "unknown.js"
+    };
+
+    let visitor = create_extract_visitor(
+        std::sync::Arc::new(metadata.source_map),
+        metadata.comments.as_ref(),
+        filename.to_string(),
+    );
+
+    program.fold_with(&mut as_folder(visitor))
+}
